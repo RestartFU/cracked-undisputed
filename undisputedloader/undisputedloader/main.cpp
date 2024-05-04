063dd0958f1488ce04ccc810a347c4a5f0a001dc
Taks <86206000+JavaNativeInterface@users.noreply.github.com>
2024-05-04 18:07:47 -0400

    small woopsie! (#2)
    * updated offsets
    
    * fixed
    
    ---------
    
    Co-authored-by: Restart <45609733+RestartFU@users.noreply.github.com>

--- undisputedloader/undisputedloader/main.cpp
+++ undisputedloader/undisputedloader/main.cpp
@@ -4,7 +4,7 @@
 #include <windows.h>
 #pragma comment (lib, "Ws2_32.lib")
 
-const char* content12 = "HTTP/1.1 200 OK\nContent-Length: 437\nContent-Type: text/plain; charset=utf-8\n\n{"reach_base_addr" : 84671456,"reach_nop_addr" : 6084600,"timer_addr" : 81664600,"velocity" : {"detour" : "f3,f,10,2,f3,f,59,5,f4,f,0,0,f3,41,f,11,40,-2,f3,f,10,42,4,f3,F,59,5,e5,f,0,0,f3,41,f,11,40,-2,f3,f,10,42,8,f3,f,59,5,ce,f,0,0,f3,41,f,11,40,-2,e9,-1,-1,-1,-1","hook" : "e9,-1,-1,-1,-1,90,f,1f,0,f,1f,40,0,f,1f,0,f,1f,40,0","sig" : "74,19,8b,2,41,89,40,-1,8b,42,4,41,89,40,-1,8b,42,8,41,89,40,-1,48,83,c4,28,c3,e8,-1,-1,-1,-1,cc,cc","vars" : "2"}}";
+const char* content12 = "HTTP/1.1 200 OK\nContent-Length: 437\nContent-Type: text/plain; charset=utf-8\n\n{\"reach_base_addr\" : 84671456,\"reach_nop_addr\" : 6084600,\"timer_addr\" : 81664600,\"velocity\" : {\"detour\" : \"f3, f, 10, 2, f3, f, 59, 5, f4, f, 0, 0, f3, 41, f, 11, 40, -2, f3, f, 10, 42, 4, f3, F, 59, 5, e5, f, 0, 0, f3, 41, f, 11, 40, -2, f3, f, 10, 42, 8, f3, f, 59, 5, ce, f, 0, 0, f3, 41, f, 11, 40, -2, e9, -1, -1, -1, -1\",\"hook\" : \"e9, -1, -1, -1, -1, 90, f, 1f, 0, f, 1f, 40, 0, f, 1f, 0, f, 1f, 40, 0\",\"sig\" : \"74, 19, 8b, 2, 41, 89, 40, -1, 8b, 42, 4, 41, 89, 40, -1, 8b, 42, 8, 41, 89, 40, -1, 48, 83, c4, 28, c3, e8, -1, -1, -1, -1, cc, cc\",\"vars\" : \"2\"}}";
 
 // sparkloader.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
